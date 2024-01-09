d4c479b0536d139c4c4671c3175f528b1f96dad5
chexq <chexq35@gmail.com>
2024-01-09 00:03:53 -0500

    react-bootstrap render

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { CardChecklist } from 'react-bootstrap-icons';
+import Container from "react-bootstrap/Container";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+import Navbar from "react-bootstrap/Navbar";
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function fetchTodos() {
+  return [
+    {
+      id: 1,
+      title: "eat dinner",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "brush teeth",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "drink water",
+      completed: true,
+    },
+    {
+      id: 4,
+      title: "take bath",
+      completed: true,
+    },
+    {
+      id: 5,
+      title: "sleep",
+      completed: false,
+    },
+  ];
+}
+
+function TodoItem(props) {
+  return (
+    <InputGroup key={props.id}>
+      <InputGroup.Checkbox checked={props.completed} />
+      <FormControl
+        value={props.title}
+        style={{
+          textDecoration: props.completed ? "line-through 4px" : "none",
+        }}
+      />
+    </InputGroup>
+  )
+}
+
+function App() {
+  const todos = fetchTodos();
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <CardChecklist />Todo List
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container>
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+          />
+        ))}
+      </Container>
+    </>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
